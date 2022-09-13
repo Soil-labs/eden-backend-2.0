7f680574735362efd8d14d621a65d86f1ec9b695
BluePanda <m.saratzidis@gmail.com>
2022-09-13 04:15:44 +0200

    feature/memberSchema_basic

--- server/graphql/schema/index.ts
+++ server/graphql/schema/index.ts
@@ -2,12 +2,14 @@ const { gql } = require('apollo-server');
 const query = require('./query.graphql');
 const mutation = require('./mutation.graphql')
 const memberType = require('./member.graphql');
+const serverType = require('./server.graphql');
 
 
 const typeDefs = gql`
 ${query}
 ${mutation}
 ${memberType}
+${serverType}
 `;
 
 export default typeDefs
