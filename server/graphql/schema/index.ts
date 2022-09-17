5da87b8c3371b25bd057201e6d9558011e01bd8c
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2022-09-17 23:39:27 +0530

    renamed typedefs

--- server/graphql/schema/index.ts
+++ server/graphql/schema/index.ts
@@ -1,22 +1,22 @@
-import memberType from "./member";
-import serverType from "./server";
-import skillType from "./skill";
-import skillSubCategoryType from "./skillSubCategory";
-import projectType from "./project";
-import commonType from "./common";
+import member from "./member";
+import server from "./server";
+import skill from "./skill";
+import skillSubCategory from "./skillSubCategory";
+import project from "./project";
+import common from "./common";
 import skillCategory from "./skillCategory";
 import rooms from "./rooms";
 import matches from "./matches";
 
 const typeDefs = [
-  ...memberType,
-  ...serverType,
-  ...skillType,
-  ...skillSubCategoryType,
-  ...projectType,
-  ...commonType,
-  ...skillCategory,
+  ...common,
+  ...member,
+  ...project,
+  ...server,
+  ...skill,
   ...rooms,
+  ...skillCategory,
+  ...skillSubCategory,
   ...matches,
 ];
 
