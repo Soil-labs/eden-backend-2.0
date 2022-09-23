48f4e5cedb5ac9830de5f6702a3f10b0c8cc1b42
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2022-09-23 17:31:47 +0530

    added findMembers Function

--- server/graphql/resolvers/members/query/findMember.ts
+++ server/graphql/resolvers/members/query/findMember.ts
@@ -1,14 +1,9 @@
 // import { Members } from "../../../../models/memberModel";
 import { ApolloError } from "apollo-server-express";
 
-const findMember = async (
-  parent: any,
-  args: { args: any; request: any },
-  context: any,
-  info: any,
-) => {
-  const { _id, serverID } = args.request;
-  console.log("_id = ", _id);
+const findMember = async (parent: any, args: { request: any }, context: any, info: any) => {
+  const { discordId, serverID } = args.request;
+  console.log("discordId = ", discordId);
   // console.log("Query > findMember > args.fields = ", args.fields);
   // if (!_id) {
   //   throw new ApolloError("No id provided");
