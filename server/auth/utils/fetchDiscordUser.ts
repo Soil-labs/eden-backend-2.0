7a3243953c66901b586b2b76039dd27fdc884f1b
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2022-09-30 10:59:31 +0530

    refactored axios request

--- server/auth/utils/fetchDiscordUser.ts
+++ server/auth/utils/fetchDiscordUser.ts
@@ -1,24 +1,36 @@
 import axios from "axios";
+import { ClientRequest } from "http";
+import QueryString from "qs";
 
 const fetchDiscordUser = async (code: string, redirect_uri: string) => {
   if (!code) throw new Error("Invalid Code supplied");
 
-  const params = new URLSearchParams();
-  params.append("client_id", process.env.DISCORD_CLIENT_ID?.toString() || "");
-  params.append("client_secret", process.env.DISCORD_CLIENT_SECRET?.toString() || "");
-  params.append("grant_type", "authorization_code");
-  params.append("code", code as string);
-  params.append("scope", "identify");
-  params.append("redirect_uri", redirect_uri);
+  let data = QueryString.stringify({
+    client_id: process.env.DISCORD_CLIENT_ID?.toString() || "",
+    client_secret: process.env.DISCORD_CLIENT_SECRET?.toString() || "",
+    grant_type: "authorization_code",
+    code: code as string,
+    scope: "identify",
+    redirect_uri: redirect_uri,
+  });
 
-  const response = await axios
-    .post(`https://discord.com/api/oauth2/token`, params.toString())
+  const config = {
+    method: "post",
+    url: "https://discord.com/api/oauth2/token",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: data,
+  };
+
+  const response = await axios(config)
+    .then(res => res.data)
     .catch((err: any) => {
-      console.error(err);
-      throw new Error("Failed to get token");
+      console.error(err.response.data);
+      throw new Error(`Failed to get token: ${err?.response?.data?.error_description}`);
     });
 
-  let { token_type, access_token } = response?.data;
+  let { token_type, access_token } = response;
 
   const authResponse = await axios
     .get(`https://discord.com/api/oauth2/@me`, {
